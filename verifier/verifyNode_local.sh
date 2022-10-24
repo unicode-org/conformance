263c6c150c2f79347597d5f202e23345cb4f242a
Craig Cornelius <ccornelius@google.com>
2022-10-24 14:57:19 -0700

    Make local scripts refer to relative path DDT_DATA

--- verifier/verifyNode_local.sh
+++ verifier/verifyNode_local.sh
@@ -1,2 +1,2 @@
 # Tests nodejs with coll_short_shift and number_fmt
-python3 verifier.py --file_base ~/ICU_conformance/conformance/DDT_DATA --exec nodejs --test_type coll_shift_short number_fmt lang_names --verify_file_name coll_verify_shift.json num_fmt_verify_file.json lang_name_verify_file.json
+python3 verifier.py --file_base ../DDT_DATA --exec nodejs --test_type coll_shift_short number_fmt lang_names --verify_file_name coll_verify_shift.json num_fmt_verify_file.json lang_name_verify_file.json
