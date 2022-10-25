1cc3c9843cd07545cd174b7e7738add98bf83ff4
Craig Cornelius <cwcornelius@gmail.com>
2022-10-25 01:08:59 +0000

    Update runColl_node_local.sh

--- testdriver/runColl_node_local.sh
+++ testdriver/runColl_node_local.sh
@@ -1,3 +1,3 @@
 # Runs collation test on only on nodejs
 # Test data and output directories are under ~/DDT_DATA
-python3 testdriver.py --test coll_shift_short --exec nodejs --run_limit 27 --file_base /usr/local/google/home/ccornelius/ICU_conformance/conformance/DDT_DATA
+python3 testdriver.py --test coll_shift_short --exec nodejs --run_limit 27 --file_base ../DDT_DATA
