356eaf33e4c62d5e55f3e93d16eb15e4f2150714
Elango Cheran <elango@unicode.org>
2023-05-12 09:55:34 -0700

    Fix typo in Rust version

--- generateDataAndRun.sh
+++ generateDataAndRun.sh
@@ -18,7 +18,7 @@ mkdir -p $TEMP_DIR/testData
 # Compile Rust executor code for ICU4X 1.0
 pushd executors/rust/
 cargo clean
-rustup install 1.6.1
+rustup install 1.61
 rustup run 1.61 cargo build --release
 popd
 
