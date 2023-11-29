74586e83a1f34745a15d5f4845eed02072bffe21
Elango Cheran <elango@unicode.org>
2023-11-29 10:53:52 -0800

    Add unit tests for basic parsing of input JSON

--- executors/icu4j/73/executor-icu4j/src/main/java/org/unicode/conformance/testtype/CollatorInputJson.java
+++ executors/icu4j/73/executor-icu4j/src/main/java/org/unicode/conformance/testtype/CollatorInputJson.java
@@ -1,5 +1,31 @@
 package org.unicode.conformance.testtype;
 
 public class CollatorInputJson implements ITestTypeInputJson {
+  public String test_type;
+
+  public String label;
+
+  public String s1;
+
+  public String s2;
+
+  public String locale;
+
+  public boolean ignorePunctuation;
+
+  public Integer line;
+
+ public String compare_type;
+
+ public String test_description;
+
+ public String[] attributes;
+
+ public String rules;
+
+ public String compare_comment;
+
+ public String warning;
+
 
 }
--- executors/icu4j/73/executor-icu4j/src/test/java/org/unicode/conformance/Icu4jExecutorTest.java
+++ executors/icu4j/73/executor-icu4j/src/test/java/org/unicode/conformance/Icu4jExecutorTest.java
@@ -1,8 +1,11 @@
 package org.unicode.conformance;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.unicode.conformance.testtype.CollatorInputJson;
+import org.unicode.conformance.testtype.CollatorOutputJson;
 
 /**
  * Unit test for simple App.
@@ -16,5 +19,37 @@ public class Icu4jExecutorTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testParseSimpleJsonInput() {
+        String testInput =
+            "{\"test_type\": \"coll_shift_short\", \"label\": \"COLL_ABC1\", \"s1\": \"de\", \"s2\" : \"da\"}";
+        CollatorInputJson input = ExecutorUtils.GSON.fromJson(testInput, CollatorInputJson.class);
+
+        assertEquals(input.test_type, "coll_shift_short");
+        assertEquals(input.label, "COLL_ABC1");
+        assertEquals(input.s1, "de");
+        assertEquals(input.s2, "da");
+    }
+
+    @Test
+    public void testFormatSimpleJsonOutput() {
+        CollatorOutputJson output = new CollatorOutputJson();
+
+    }
+
+    @Test
+    public void testSimpleJsonInputRoundTrip() {
+        String testInput =
+            "{\"test_type\": \"coll_shift_short\", \"label\": \"COLL_ABC1\", \"s1\": \"de\", \"s2\" : \"da\"}";
+        CollatorInputJson input = ExecutorUtils.GSON.fromJson(testInput, CollatorInputJson.class);
+        String formattedString = ExecutorUtils.GSON.toJson(input);
+        CollatorInputJson reparsedInput = ExecutorUtils.GSON.fromJson(formattedString, CollatorInputJson.class);
+        String reformattedString = ExecutorUtils.GSON.toJson(reparsedInput);
+
+        assertEquals("Canonical JSON string should round trip",
+            formattedString,
+            reformattedString);
     }
 }
