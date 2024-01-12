4da41900663a094e4ede9ddd8adf36ae1d36bf67
Elango Cheran <elango@unicode.org>
2024-01-11 16:16:44 -0800

    Add Likely Subtags component tests to ICU4J executor

---
+++ executors/icu4j/73/executor-icu4j/src/main/java/org/unicode/conformance/testtype/likelysubtags/LikelySubtagsInputJson.java
@@ -0,0 +1,15 @@
+package org.unicode.conformance.testtype.likelysubtags;
+
+import org.unicode.conformance.testtype.ITestTypeInputJson;
+
+public class LikelySubtagsInputJson implements ITestTypeInputJson {
+
+  public String test_type;
+
+  public String label;
+
+  public String locale;
+
+  public LikelySubtagsTestOption option;
+
+}
