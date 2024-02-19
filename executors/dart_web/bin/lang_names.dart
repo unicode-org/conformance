d3495ed00c5b788dc47386e40147d8fe9ab75e17
Craig <ccornelius@google.com>
2024-02-18 21:49:21 -0800

    Fix some problems with dart_web lang_names

--- executors/dart_web/bin/lang_names.dart
+++ executors/dart_web/bin/lang_names.dart
@@ -6,18 +6,33 @@ import 'package:intl4x/intl4x.dart';
 String testLangNames(String jsonEncoded) {
   final json = jsonDecode(jsonEncoded) as Map<String, dynamic>;
 
-  final Locale locale;
-  if (json['locale_label'] != null) {
-    // Fix to use dash, not underscore.
-    final localeJson = json['locale_label'] as String;
-    locale = Locale.parse(localeJson.replaceAll('/_/g', '-'));
-  } else {
-    locale = Locale(language: 'en');
-  }
-  final languageLabel =
-      (json['language_label'] as String).replaceAll('/_/g', '-');
-
   final outputLine = <String, dynamic>{};
+
+  final Locale locale;
+  try {
+    if (json['locale_label'] != null) {
+      // Fix to use dash, not underscore.
+      final localeJson = json['locale_label'] as String;
+      locale = Locale.parse(localeJson.replaceAll('/_/g', '-'));
+    } else {
+      locale = Locale(language: 'en');
+    }
+  } catch(error) {
+    outputLine.addAll({
+        'error': 'locale_label: ' + error.toString(),
+        'label': json['label'],
+        'test_type': 'display_names',
+        'error_type': 'unsupported',
+        'error_detail': 'locale_label',
+        'error_retry': false // Do not repeat
+
+    });
+    return jsonEncode(outputLine);
+  },
+
+  final languageLabel =
+  (json['language_label'] as String).replaceAll('/_/g', '-');
+
   try {
     final options = DisplayNamesOptions(
       languageDisplay: LanguageDisplay.standard,
@@ -29,13 +44,15 @@ String testLangNames(String jsonEncoded) {
     outputLine['result'] = resultLocale;
   } catch (error) {
     outputLine.addAll({
-      'error': error.toString(),
-      'label': json['label'],
-      'locale_label': locale.toLanguageTag(),
-      'language_label': languageLabel,
-      'test_type': 'display_names',
-      'error_type': 'unsupported',
-      'error_retry': false // Do not repeat
+        //      'error': error.toString() + " language_label:" + languageLabel,
+        'error': 'something went wrong: ', + error.toString(),
+        'label': json['label'],
+        'locale_label': locale.toLanguageTag(),
+        'language_label': languageLabel,
+        'test_type': 'display_names',
+        'error_type': 'unsupported',
+        'error_detail': languageLabel,
+        'error_retry': false // Do not repeat
     });
   }
   return jsonEncode(outputLine);
