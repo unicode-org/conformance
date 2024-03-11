27ab673144b8a3b1bd5ed5ec0fcff97aaebe0f2a
Craig <ccornelius@google.com>
2024-03-11 16:27:59 -0700

    Starting DateTime generate parsing

--- testgen/generators/datetime_fmt.py
+++ testgen/generators/datetime_fmt.py
@@ -3,12 +3,35 @@ import os
 import json
 import logging
 import re
+import tokenize
+
 from generators.base import DataGenerator
 
 class DateTimeFmtGenerator(DataGenerator):
     def process_test_data(self):
+        result = None
 
-        # Get this file and tokenize if
+        filename = "format.txt"
+        filepath = os.path.join(self.icu_version, filename)
 
-        # Remove comments: DOUBLESLASH through NL (new line)
+        # Get this file and tokenize it.
+        tokens = None
+        with tokenize.open(filepath) as f:
+            tokens = tokenize.generate_tokens(f.readline)
 
+            done = False
+            while not done:
+                try:
+                    item = next(tokens)
+                except StopIteration:
+                    done = true
+
+                if item['string'] == 
+                print(item)
+
+            # Now we should have the tokens.
+            for token in tokens:
+                print(token)
+            # Remove comments: DOUBLESLASH through NL (new line)
+
+        return result
