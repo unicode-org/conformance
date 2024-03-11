d86eaaec4ed6fd29412055254a8e2b1da112898c
Craig <ccornelius@google.com>
2024-03-11 16:26:38 -0700

    Starting DateTime generate parsing

---
+++ testgen/generators/datetime_fmt.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import logging
+import re
+from generators.base import DataGenerator
+
+class DateTimeFmtGenerator(DataGenerator):
+    def process_test_data(self):
+
+        # Get this file and tokenize if
+
+        # Remove comments: DOUBLESLASH through NL (new line)
+
