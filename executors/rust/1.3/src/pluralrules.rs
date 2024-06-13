7863205024f49fc0d0209b6782fe0cbff10752e6
Craig <ccornelius@google.com>
2024-06-13 13:10:25 -0700

    Fix formatting

--- executors/rust/1.3/src/pluralrules.rs
+++ executors/rust/1.3/src/pluralrules.rs
@@ -12,17 +12,15 @@ pub fn run_plural_rules_test(json_obj: &Value) -> Result<Value, String> {
 
     let locale_str: &str = json_obj["locale"].as_str().unwrap();
 
-    let locale =
-        if let Ok(lc) = locale_str.parse::<Locale>() {
-            lc
-        } else {
-            return Ok(json!({
-                "label": label,
-                "error_detail": {"option": locale_str},
-                "error_type": "locale problem",
-            }));
-        };
-
+    let locale = if let Ok(lc) = locale_str.parse::<Locale>() {
+        lc
+    } else {
+        return Ok(json!({
+            "label": label,
+            "error_detail": {"option": locale_str},
+            "error_type": "locale problem",
+        }));
+    };
 
     // Get number string
     let input_number = &json_obj["sample"].as_str().unwrap();
