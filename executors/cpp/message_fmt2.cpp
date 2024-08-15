c02eeb1a5ecde3e540928c73914a2a3c27e8db9b
Craig <ccornelius@google.com>
2024-08-15 16:54:36 -0700

    MF2 in ICU4C is compiling!

--- executors/cpp/message_fmt2.cpp
+++ executors/cpp/message_fmt2.cpp
@@ -8,10 +8,14 @@
 #include <unicode/messageformat2.h>
 #include <unicode/utypes.h>
 
-#include "unicode/messageformat2_arguments.h"
-#include "unicode/messageformat2_data_model.h"
-#include "unicode/messageformat2_function_registry.h"
-#include "unicode/unistr.h"
+
+#include <unicode/messageformat2_arguments.h>
+#include <unicode/messageformat2_data_model.h>
+#include <unicode/messageformat2_function_registry.h>
+#include <unicode/messageformat2_formattable.h>
+#include <unicode/messageformat2.h>
+#include <unicode/msgfmt.h>
+#include <unicode/unistr.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,14 +25,22 @@
 #include <cstring>
 
 using icu::Locale;
-using namespace icu::message2;
+using icu::message2::MessageFormatter;
+using icu::message2::MessageArguments;
+using icu::message2::Formattable;
+
+using icu::UnicodeString;
+
 
 using std::cout;
 using std::endl;
 using std::string;
 
+/* Based on this test file:
+ * https://github.com/unicode-org/icu/blob/main/icu4c/source/test/intltest/messageformat2test.cpp
+ */
 const string TestMessageFormat2(json_object *json_in) {
-  UErrorCode status = U_ZERO_ERROR;
+  UErrorCode errorCode = U_ZERO_ERROR;
 
   // label information
   json_object *label_obj = json_object_object_get(json_in, "label");
@@ -50,16 +62,18 @@ const string TestMessageFormat2(json_object *json_in) {
 
 
   // Get all the argements from test params
-  std::map<UnicodeString, message2::Formattable> argsBuilder;
-  argsBuilder["userName"] = message2::Formattable("John");
+  std::map<UnicodeString, Formattable> argsBuilder;
+  argsBuilder["userName"] = Formattable("John");
   MessageArguments args(argsBuilder, errorCode);
 
   json_object *param_list_obj = json_object_object_get(json_in, "params");
 
   string params_name;
+  UnicodeString u_params_name;
   string params_value;
+  UnicodeString u_params_value;
 
-  if (param_lis_obj) {
+  if (param_list_obj) {
     // For each item in the params list, get the name and value.
     // Add to the builder.
     int params_length = json_object_array_length(param_list_obj);
@@ -71,22 +85,25 @@ const string TestMessageFormat2(json_object *json_in) {
       json_object *params_name_obj = json_object_object_get(param_obj, "name");
       if (params_name_obj) {
         params_name = json_object_get_string(params_name_obj);
+        u_params_name = params_name.c_str();
       }
       json_object *params_value_obj = json_object_object_get(param_obj, "value");
       if (params_value_obj) {
         params_value = json_object_get_string(params_value_obj);
+        u_params_value = params_value.c_str();
       }
-      argsBuilder[params_name] = message::Formattable(params_value);
+      argsBuilder[u_params_name] = Formattable(u_params_value);
     }
   }
-  MessageArguments args(argsBuilder, errorCode);
 
   // Start filling the return data.
   json_object *return_json = json_object_new_object();
   json_object_object_add(return_json, "label", label_obj);
 
-  if (U_FAILURE(status)) {
-    const char* error_name = u_errorName(status);
+  bool no_error = true;
+
+  if (U_FAILURE(errorCode)) {
+    const char* error_name = u_errorName(errorCode);
     json_object_object_add(
         return_json,
         "error", json_object_new_string("error in constructor"));
@@ -96,14 +113,16 @@ const string TestMessageFormat2(json_object *json_in) {
     no_error = false;
   }
 
+  UParseError parseError;
+
   MessageFormatter::Builder builder(errorCode);
   MessageFormatter mf = builder.setPattern(u"Hello, {$userName}!", parseError, errorCode)
-        .build(errorCode);
+                        .build(errorCode);
 
-  UnicodeString result_ustring = formatter.formatToString(arguments, status);
+  UnicodeString result_ustring = mf.formatToString(args, errorCode);
   // Call the function and return the result.
-  if (U_FAILURE(status)) {
-    const char* error_name = u_errorName(status);
+  if (U_FAILURE(errorCode)) {
+    const char* error_name = u_errorName(errorCode);
     json_object_object_add(
         return_json,
         "error", json_object_new_string("error in formatToString"));
@@ -111,25 +130,24 @@ const string TestMessageFormat2(json_object *json_in) {
         return_json,
         "error_detail", json_object_new_string(error_name));
     no_error = false;
-    }
+  }
 
 
   int32_t chars_out;  // Results of extracting characters from Unicode string
 
-  bool no_error = true;
   char test_result[1000] = "";
 
   // Get the resulting value as a string
-  result_ustring.extract(test_result, 1000, nullptr, status);  // ignore result
+  result_ustring.extract(test_result, 1000, nullptr, errorCode);  // ignore result
 
   // Call the function and return the result.
-  if (U_FAILURE(status)) {
-      } else {
-      // It worked!
-      json_object_object_add(return_json,
-                             "result",
-                             json_object_new_string(test_result));
-    }
+  if (U_FAILURE(errorCode)) {
+  } else {
+    // It worked!
+    json_object_object_add(return_json,
+                           "result",
+                           json_object_new_string(test_result));
+  }
 
   string return_string = json_object_to_json_string(return_json);
   return return_string;
