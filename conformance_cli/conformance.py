79ada6017f0fc3a97f34294ed2f6791f5f0ccc84
Matt Radbourne <1254508+mradbourne@users.noreply.github.com>
2024-10-09 00:30:36 +0100

    Use subprocess for unified CLI

--- conformance_cli/conformance.py
+++ conformance_cli/conformance.py
@@ -1,18 +1,18 @@
 import argparse
+import subprocess
 import sys
 from testgen import testdata_gen
-from testdriver import testdriver, ddtargs
+from testdriver import ddtargs
 
 
 def main():
     args = parse_args()
 
     if args.command == "testgen":
-        testdata_gen.main(sys.argv[1:])
+        subprocess.run(["python3", "testdata_gen.py"] + sys.argv[2:], cwd="testgen")
 
     if args.command == "test":
-        # FIXME
-        testdriver.main(sys.argv[1:])
+        subprocess.run(["python3", "testdriver.py"] + sys.argv[2:], cwd="testdriver")
 
 
 def parse_args():
@@ -24,6 +24,7 @@ def parse_args():
     testdata_gen.setup_args(testgen_parser)
 
     test_parser = subparsers.add_parser("test")
-    ddtargs.setup_args(test_parser)
+    ddtargs.setup_common_args(test_parser)
+    ddtargs.setup_testdriver_args(test_parser)
 
     return parser.parse_args()
--- testgen/config.py
+++ testgen/config.py
@@ -1,13 +1,6 @@
-from os import getcwd
-from os.path import exists, join
+from os.path import join
 
-_unified_cli_paths = {
-    "logging_config": "logging.conf",
-    "default_input": join("DDT_DATA", "testData"),
+paths = {
+    "logging_config": join("..", "logging.conf"),
+    "default_input": join("..", "DDT_DATA", "testData"),
 }
-
-_legacy_paths = {key: join("..", value) for key, value in _unified_cli_paths.items()}
-
-using_unified_cli = exists(join(getcwd(), "conformance"))
-
-paths = _unified_cli_paths if using_unified_cli else _legacy_paths
