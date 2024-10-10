a0ae1625554f0e0493cf1bb37f96de8789ae3c2c
Elango Cheran <elango@unicode.org>
2024-10-10 16:18:12 -0700

    Set NVM_DIR conditionally based on style (version?) of installation used

--- generateDataAndRun.sh
+++ generateDataAndRun.sh
@@ -24,9 +24,11 @@ fi
 if [[ $CI == "true" ]] && ! [ -x "$(command -v nvm)" ]
 then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    export NVM_DIR="$HOME/.config/nvm
+else
+    export NVM_DIR=$HOME/.nvm
 fi
 
-export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
 
 ##########
