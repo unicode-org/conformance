2c70792d35e520d079aa3203051add07d8bd431a
Elango Cheran <elango@unicode.org>
2024-10-10 16:29:41 -0700

    Add steps from NVM Readme installation section on loading NVM

--- generateDataAndRun.sh
+++ generateDataAndRun.sh
@@ -25,6 +25,8 @@ if [[ $CI == "true" ]] && ! [ -x "$(command -v nvm)" ]
 then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
     export NVM_DIR="$HOME/.config/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 else
     export NVM_DIR=$HOME/.nvm
 fi
