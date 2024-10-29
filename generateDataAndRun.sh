2ad783b315a067136e0914cd2654bb03001bc2ee
Craig <ccornelius@google.com>
2024-10-29 13:44:34 -0700

    Get rid of debugging for nvm existence

--- generateDataAndRun.sh
+++ generateDataAndRun.sh
@@ -21,19 +21,6 @@ fi
 # Enable seting the version of NodeJS
 # Install NVM if it is not install in CI
 
-echo "test whether nvm is on the command line"
-
-if [ -x "$(command -v nvm)" ]
-then
-  echo "nvm is in the path"
-  echo "output from running nvm without args"
-  nvm
-else
-  echo "nvm is NOT in the PATH"
-fi
-
-
-
 export NVM_DIR=$HOME/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
