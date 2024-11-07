b3900bc8e9779b7a4e4530a8af821a848b1e928c
Craig Cornelius <ccornelius@google.com>
2024-11-06 16:56:37 -0800

    Clang tidy - apply  (#326)
    * Add plural rules to CPP
    
    * Clang-tidy applied to *.cpp
    
    * More style fixes
    
    * DDT-234 Apply clang-tidy to *.cpp
    
    * Added Clang Tidy to CI
    
    * Apply changes from clang-tidy
    
    * Make icu4c setup script more robust
    
    * Add a clang `compile_commands.txt` to assist compilation output msgs
    
    * Apply clang-tidy change
    
    * Add clang-tidy config file
    
    * Test clang-tidy run on CI to sidestep CI upload step permissions
    
    * Mod to workaround GHA permissions
    
    * Try to include json-c dependency in clang-tidy CI action
    
    * Refactor clang-tidy CI jobs into a separate workflow
    
    * Update CI job names
    
    * Change workflow name to trigger the 2nd workflow
    
    * Remove second workflow b/c it won't trigger without user PAT tokens on a PR branch
    
    * test CI by inducing a known error
    
    * Revert "test CI by inducing a known error"
    
    This reverts commit d7a90ffdf0d5e7e7e8e2ef86c23ed7fc3a567eee.
    
    * Add CLI usage for clang-tidy locally
    
    ---------
    
    Co-authored-by: Elango Cheran <elango@unicode.org>

--- executors/cpp/util.h
+++ executors/cpp/util.h
@@ -8,7 +8,7 @@
 
 using std::string;
 
-extern const bool check_icu_error(UErrorCode error_code,
+extern auto check_icu_error(UErrorCode error_code,
                                   json_object *return_json,
-                                  string message_to_add_if_error);
+                                  string message_to_add_if_error) -> const bool;
 
