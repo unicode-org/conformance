b9b3e5dcd5b6ec88ad6208fe404dce2405284cef
Shane F. Carr <shane@unicode.org>
2024-12-17 16:21:43 -0800

    Add ICU4X 2.0-beta1 code files

---
+++ executors/rust/2.0-beta1/src/likelysubtags.rs
@@ -0,0 +1,44 @@
+//! Provides tests for likely subtags to mimimize and maximize.
+
+use serde_json::{json, Value};
+
+use icu::locale::LanguageIdentifier;
+use icu::locale::LocaleExpander;
+
+// https://docs.rs/icu/latest/icu/locid_transform/
+
+// Function runs language names tests
+pub fn run_likelysubtags_test(json_obj: &Value) -> Result<Value, String> {
+    let lc = LocaleExpander::new_extended();
+
+    let label = &json_obj["label"].as_str().unwrap();
+
+    let test_option = &json_obj["option"].as_str().unwrap();
+
+    let locale_str: &str = json_obj["locale"].as_str().unwrap();
+
+    let mut locale = locale_str.parse::<LanguageIdentifier>().unwrap();
+
+    if test_option == &"minimizeFavorRegion" {
+        // This option is not yet supported.
+        return Ok(json!({
+            "label": label,
+            "error_detail": {"option": test_option},
+            "unsupported": "minimizeFavorRegion",
+            "error_type": "unsupported",
+        }));
+    }
+
+    if test_option == &"minimize" {
+        lc.minimize(&mut locale);
+    } else if test_option == &"maximize" {
+        lc.maximize(&mut locale);
+    }
+
+    let json_result = json!({
+        "label": label,
+        "result": locale.to_string()
+    });
+
+    Ok(json_result)
+}
