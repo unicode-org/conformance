b17daa9436055d938e4a13c7ce817942b5193f01
Craig Cornelius <ccornelius@google.com>
2025-01-06 17:35:03 -0800

    Removing old target DDT_DATA (#376)

--- testdriver/runAll_rust_local.sh
+++
@@ -1,3 +0,0 @@
-# Runs collation test on only on rust
-# Test data and output directories are under ~/DDT_DATA
-python3 testdriver.py  --icu_version icu71 --test coll_shift_short number_fmt --exec rust --file_base ../DDT_DATA
