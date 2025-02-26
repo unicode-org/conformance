60561424a6aa47b1810aaa5696fe58400361471e
Craig Cornelius <ccornelius@google.com>
2025-02-26 13:38:42 -0800

    Reclassifying some failures as unsupported instead of errors (#393)
    * Reclassifying some failures as unsupported instead of errors
    
    * Looking at likely subtags unsupported
    
    * Dart fixes
    
    ---------
    
    Co-authored-by: Moritz <mosum@google.com>

--- executors/dart_web/bin/lang_names.dart
+++ executors/dart_web/bin/lang_names.dart
@@ -6,7 +6,7 @@ import 'package:intl4x/intl4x.dart';
 String testLangNames(String jsonEncoded) {
   final json = jsonDecode(jsonEncoded) as Map<String, dynamic>;
 
-  final outputLine = <String, dynamic>{};
+  final outputLine = <String, dynamic>{'label': json['label']};
 
   final Locale locale;
   try {
@@ -17,41 +17,43 @@ String testLangNames(String jsonEncoded) {
     } else {
       locale = Locale(language: 'en');
     }
-  } catch(error) {
+  } catch (error) {
     outputLine.addAll({
-        'error': 'locale_label: ' + error.toString(),
-        'label': json['label'],
-        'test_type': 'display_names',
-        'error_type': 'unsupported',
-        'error_detail': 'locale_label',
-        'error_retry': false // Do not repeat
+      'error_detail': 'locale_label: $error',
+      'error_type': 'unsupported',
+      'error_retry': false // Do not repeat
     });
     return jsonEncode(outputLine);
   }
 
-  final languageLabel =
-  (json['language_label'] as String).replaceAll('_', '-');
-
+  final languageLabel = (json['language_label'] as String).replaceAll('_', '-');
+  Locale languageLabelLocale;
   try {
-    final options = DisplayNamesOptions(
-      languageDisplay: LanguageDisplay.standard,
-    );
-    final displayNames = Intl(locale: locale).displayNames(options);
-    final resultLocale = displayNames.ofLanguage(Locale.parse(languageLabel));
+    languageLabelLocale = Locale.parse(languageLabel);
+  } catch (error) {
+    // Something was not supported in this locale identifier
+    outputLine.addAll({
+      'error_type': 'unsupported',
+      'error_detail': error.toString(),
+      'error_retry': false // Do not repeat
+    });
+    return jsonEncode(outputLine);
+  }
 
-    outputLine['label'] = json['label'];
-    outputLine['result'] = resultLocale;
+  final options = DisplayNamesOptions(
+    languageDisplay: LanguageDisplay.standard,
+  );
+  try {
+    final displayNames = Intl(locale: locale).displayNames(options);
+    final resultLangName = displayNames.ofLanguage(languageLabelLocale);
+
+    outputLine['result'] = resultLangName;
   } catch (error) {
     outputLine.addAll({
-        //      'error': error.toString() + " language_label:" + languageLabel,
-        'error': 'something went wrong: ' + error.toString(),
-        'label': json['label'],
-        'locale_label': locale.toLanguageTag(),
-        'language_label': languageLabel,
-        'test_type': 'display_names',
-        'error_type': 'unsupported',
-        'error_detail': languageLabel,
-        'error_retry': false // Do not repeat
+      'error_type': 'unsupported',
+      'error_detail': error.toString(),
+      'actual_options': options,
+      'error_retry': false // Do not repeat
     });
   }
   return jsonEncode(outputLine);
