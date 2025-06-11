8363a74ff12061acbad81b92183dc271f22af618
sven-oly <cwcornelius@gmail.com>
2025-06-11 16:45:10 -0700

    ICU4X segmenter in beta2.02 and later

--- executors/rust/src/segmenter.rs
+++ executors/rust/src/segmenter.rs
@@ -3,16 +3,10 @@
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
-#[cfg(not(any(ver = "1.3", ver = "1.4", ver = "1.5", ver = "2.0-beta1")))]
-use icu::segmenter::{
-    GraphemeClusterSegmenter,
-    options::WordBreakInvariantOptions, WordSegmenter,
-    options::SentenceBreakInvariantOptions, SentenceSegmenter,
-    LineSegmenter
- };
-use super::compat::{pref, Locale};
+#[cfg(not(any(ver = "2.0-beta1")))]
+use icu::segmenter::{options::*, *};
 
-use writeable::Writeable;
+use super::compat::Locale;
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "snake_case")]
@@ -20,52 +14,77 @@ struct SegmenterOptions {
     granularity: Option<String>,
 }
 
-
 // Function runs segmenter tests
-pub fn run_segmenter_test(json_obj: &Value) -> Result<Value, Vec<str>> {
+pub fn run_segmenter_test(json_obj: &Value) -> Result<Value, String> {
+    // To use the locale
     let label = &json_obj["label"].as_str().unwrap();
 
     let locale_str: &str = json_obj["locale"].as_str().unwrap();
     let locale = locale_str.parse::<Locale>().unwrap();
+    let lang_identifier = locale.id;
 
     let options = &json_obj["options"]; // This will be an array.
     let option_struct: SegmenterOptions = serde_json::from_str(&options.to_string()).unwrap();
 
     let granularity: &str = option_struct.granularity.as_ref().unwrap();
 
-    let input_str: &str = json_obj["input"].as_str().unwrap();
+    let input_string: &str = json_obj["input"].as_str().unwrap();
 
-    let segmenter = match granularity {
-        "grapheme_cluster" => GraphemeClusterSegmenter::new(),
-        "word" => WordSegmenter::new_auto(WordBreakInvariantOptions::default()),
-        "sentence" => SentenceSegmenter::new(SentenceBreakInvariantOptions::default()),
-        "line" => LineSegmenter::new_auto(Default::default()),
-        '_' => None  // This is an error.
-    }
-
-    // Get the break points first.
-    let breakpoints: Vec<usize> = segmenter
-        .segment_str(input_str)
-        .collect();
-
-    let json_result = match breakpoints {
-        Ok(formatter) => {
-            // Create output as list of strings broken ad the breakpoints.
-            let result = Vec<str>;
-            json!({
-                "label": label,
-                "result":  
-            })
+    // Get desired segmenter, then comput the break points from the input.
+    let breakpoints: Vec<usize> = match granularity {
+        "grapheme_cluster" => {
+            let segmenter = GraphemeClusterSegmenter::new();
+            segmenter.segment_str(input_string).collect()
         }
-        Err(e) => {
-            json!({
+        "word" => {
+            // Get options
+            let mut options = WordBreakOptions::default();
+            options.content_locale = Some(&lang_identifier);
+
+            let segmenter = WordSegmenter::try_new_auto(options).unwrap();
+            segmenter.segment_str(input_string).collect()
+        }
+        "sentence" => {
+            // Get options
+            let mut options = SentenceBreakOptions::default();
+            options.content_locale = Some(&lang_identifier);
+            let segmenter = SentenceSegmenter::try_new(options).unwrap();
+            segmenter.segment_str(input_string).collect()
+        }
+        "line" => {
+            // Get options
+            let mut options = LineBreakOptions::default();
+            options.strictness = Some(LineBreakStrictness::Strict);
+            options.word_option = Some(LineBreakWordOption::BreakAll);
+            options.content_locale = Some(&lang_identifier);
+            let segmenter = LineSegmenter::new_auto(options);
+            segmenter.segment_str(input_string).collect()
+        }
+        _ => {
+            // This is an error
+            return Ok(json!({
                 "label": label,
                 "locale_label": locale_str,
-                "error": e.to_string(),
+                "error": "Unknown segmenter option",
                 "error_type": "unsupported",
-                "unsupported": e.to_string(),
+                "unsupported": granularity.to_string(),
                 "error_detail": {"unsupported_locale": locale_str}
-            })
+            }));
         }
-    };    
+    };
+
+    // Create output as list of strings broken ad the breakpoints.
+    // For breakpoints, extract each part and push to result;
+    let mut segments: Vec<String> = vec![];
+    let mut start: usize = 0;
+    for index in breakpoints.iter() {
+        if *index > 0 {
+            segments.push(input_string[start..*index].to_string());
+        }
+        start = *index;
+    }
+    Ok(json!({
+        "label": label,
+        "result": segments
+    }))
 }
