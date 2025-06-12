158de3c1efba978d99991f386d8127fbd735bff7
Craig Cornelius <ccornelius@google.com>
2025-06-12 07:40:10 -0700

    ICU4X Segmenter now working for 2.0 beta2 (#465)
    * Starting Segmenter tests in ICU4X
    
    * ICU4X segmenter in beta2.02 and later
    
    * Remove option to break lines within words

--- executors/rust/src/mod.rs
+++ executors/rust/src/mod.rs
@@ -10,6 +10,8 @@ pub mod localenames;
 pub mod numberfmt;
 pub mod pluralrules;
 pub mod relativedatetime_fmt;
+#[cfg(not(any(ver = "1.3", ver = "1.4", ver = "1.5", ver = "2.0-beta1")))]
+pub mod segmenter;
 
 #[cfg(any(ver = "1.3", ver = "1.4", ver = "1.5"))]
 #[path = "datetime_1.rs"]
