de0022c33c18f8844ab86b39feb3bb7c4cc6bc49
Craig Cornelius <ccornelius@google.com>
2025-07-16 12:31:56 -0700

    ICU4X collation - mark rules as unsupported. Apply strength option. (#479)
    * ICU4X collation - mark rules as unsupported. Apply strength option.
    
    * Cargo fmt
    
    * Update collator.rs
    
    * Fix detail copy on left click to unescape HTML entitiees
    
    * Add comment to fix locale handling
    
    * fmt

--- executors/rust/src/collator.rs
+++ executors/rust/src/collator.rs
@@ -18,12 +18,50 @@ pub fn run_collation_test(json_obj: &Value) -> Result<Value, String> {
     let str1: &str = json_obj["s1"].as_str().unwrap();
     let str2: &str = json_obj["s2"].as_str().unwrap();
 
+    // This may be missing
+    let compare_option: Option<&str> = json_obj["compare_type"].as_str();
+
+    let strength_option: Option<&str> = json_obj["strength"].as_str();
+
+    let rules: Option<&str> = json_obj["rules"].as_str();
+
     #[cfg(any(ver = "1.3", ver = "1.4", ver = "1.5"))]
     let mut options = CollatorOptions::new();
     #[cfg(not(any(ver = "1.3", ver = "1.4", ver = "1.5")))]
     let mut options = CollatorOptions::default();
 
-    options.strength = Some(Strength::Tertiary);
+    // TODO: Get and apply locale if given. Else use "und" or "en"
+
+    // Rules not yet supported.
+    if rules.is_some() {
+        return Ok(json!({
+            "label": label,
+            "error_detail": "Rules not supported",
+            "unsupported": "Collator rules not available",
+            "error_type": "unsupported",
+        }));
+    }
+
+    // Use compare_type to get < or =.
+    let compare_symbol = compare_option.and_then(|c| c.get(0..1)).unwrap_or("<");
+
+    if let Some(strength) = strength_option {
+        options.strength = match strength {
+            "primary" => Some(Strength::Primary),
+            "secondary" => Some(Strength::Secondary),
+            "tertiary" => Some(Strength::Tertiary),
+            "quaternary" => Some(Strength::Quaternary),
+            "identical" => Some(Strength::Identical),
+            _ => {
+                return Ok(json!({
+                    "label": label,
+                    "error_detail": {"strength": strength},
+                    "unsupported": "strength",
+                    "error_type": "unsupported",
+                }));
+            }
+        };
+    };
 
     // Ignore punctuation only if using shifted test.
     if let Some(ip) = ignore_punctuation {
@@ -32,11 +70,27 @@ pub fn run_collation_test(json_obj: &Value) -> Result<Value, String> {
         }
     }
 
+    // TODO !! Iterate to find actual level of comparison, then look
+    // at compare type (1, 2, 3, 4, i, c) to see if it matches
+
     let collator = Collator::try_new(pref!(locale!("en")), options).unwrap();
 
     let comparison = collator.compare(str1, str2);
 
-    let result_string = comparison.is_le();
+    let result_string = match compare_symbol {
+        "<" => comparison.is_le(),
+        "=" => comparison.is_eq(),
+        _ => {
+            return Ok(json!({
+                "label": label,
+                "error_detail": {
+                    "compare_type": compare_symbol,
+                },
+                "unsupported": "compare_symbol",
+                "error_type": "unsupported",
+            }));
+        }
+    };
 
     let mut comparison_number: i16 = 0;
     if comparison == Ordering::Less {
@@ -44,6 +98,7 @@ pub fn run_collation_test(json_obj: &Value) -> Result<Value, String> {
     } else if comparison == Ordering::Greater {
         comparison_number = 1;
     }
+
     // TODO: Convert comparison to "<", "=", or ">"
     let json_result = json!({
         "label": label,
