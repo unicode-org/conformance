8d128cec1d4de5a1b0fc152216a3edabffc2a69a
Moritz <mosum@google.com>
2025-07-28 09:18:31 +0200

    fix?

--- generateDataAndRun.sh
+++ generateDataAndRun.sh
@@ -91,7 +91,7 @@ then
     pushd executors/dart/
     dart pub get
     dart bin/set_version.dart
-    dart build cli bin/executor.dart
+    dart build cli --target bin/executor.dart
     popd
 fi
 
